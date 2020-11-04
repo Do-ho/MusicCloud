6491440e761119aa3adadc57c6ac10dcf503ef91
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-04 09:21:57 +0900

    chore: Front-end 개발환경 설정
    - CRA를 활용해 빠른 프로젝트 생성

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <h1> Music Cloud </h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;

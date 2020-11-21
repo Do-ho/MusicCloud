354f671732b55fbd3ec3f393125a103b6feae874
UiHyeon <yh4268@koreatech.ac.kr>
2020-11-21 15:25:25 +0900

    DB연동 및 얼굴 사진 감정분석 API test

--- server/app.py
+++ server/app.py
@@ -1,55 +1,77 @@
 from flask import Flask
+from flask import render_template
+from db import mysql
 import json
 import requests
 
 app = Flask(__name__)
-
-# 공통 데이터(ID, Secret)
-ID = "insert your ID" # 인증 정보의 Client ID
-Secret = "insert your secret" # 인증 정보의 Client Secret
-
-# CSR (음성 인식 API 사용 코드)
-csrdata = open("./voice/test.mp3", "rb") # STT를 진행하고자 하는 음성 파일
-
-Lang = "Kor" # Kor / Jpn / Chn / Eng
-csrURL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + Lang
-    
-csrheaders = {
-    "Content-Type": "application/octet-stream", # Fix
-    "X-NCP-APIGW-API-KEY-ID": ID,
-    "X-NCP-APIGW-API-KEY": Secret,
-}
-
-csrresponse = requests.post(csrURL,  data=csrdata, headers=csrheaders)
-csrrescode = csrresponse.status_code
+db = mysql.Testdb()
 
 
-# CFR(얼굴 인식 API 사용 코드)
-cfrdata = {'image': open('./images/이동욱.jpg', 'rb')} # 얼굴감지 데이터 파일
+@app.route("/")
+def hello():
+    result = db.select_all()
 
-cfrURL = "https://naveropenapi.apigw.ntruss.com/vision/v1/face"
+    text = str(result[0][0])
 
-cfrheaders = {
-    "X-NCP-APIGW-API-KEY-ID": ID,
-    "X-NCP-APIGW-API-KEY": Secret,
-}
+    return text
 
-cfrresponse = requests.post(cfrURL,  files=cfrdata, headers=cfrheaders)
-cfrrescode = cfrresponse.status_code
 
-#json 파싱
-jsonObject = json.loads(cfrresponse.text)
-jsonArray = jsonObject.get("faces")
+@app.route("/home")
+def home():
+    return render_template("index.html")
 
-# 
-if(cfrrescode == 200):
-    for list in jsonArray:
-        print (list.get("emotion").get("value"))
-        print(csrresponse.text)
-    #@app.route('/')
-    #def index():
-    #    return emotion
-else:
-    print("Error : " + cfrresponse.text)
 
-    
+@app.route("/testApi")
+def testApi():
+    ID = "*" # 인증 정보의 Client ID
+    Secret = "*" # 인증 정보의 Client Secret
+
+    # CSR (음성 인식 API 사용 코드)
+    csrdata = open("./static/voice/test.mp3", "rb") # STT를 진행하고자 하는 음성 파일
+
+    Lang = "Kor" # Kor / Jpn / Chn / Eng
+    csrURL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + Lang
+        
+    csrheaders = {
+        "Content-Type": "application/octet-stream", # Fix
+        "X-NCP-APIGW-API-KEY-ID": ID,
+        "X-NCP-APIGW-API-KEY": Secret,
+    }
+
+    csrresponse = requests.post(csrURL,  data=csrdata, headers=csrheaders)
+    csrrescode = csrresponse.status_code
+
+
+    # CFR(얼굴 인식 API 사용 코드)
+    cfrdata = {'image': open('./static/img/이동욱.jpg', 'rb')} # 얼굴감지 데이터 파일
+
+    cfrURL = "https://naveropenapi.apigw.ntruss.com/vision/v1/face"
+
+    cfrheaders = {
+        "X-NCP-APIGW-API-KEY-ID": ID,
+        "X-NCP-APIGW-API-KEY": Secret,
+    }
+
+    cfrresponse = requests.post(cfrURL,  files=cfrdata, headers=cfrheaders)
+    cfrrescode = cfrresponse.status_code
+
+    #json 파싱
+    jsonObject = json.loads(cfrresponse.text)
+    jsonArray = jsonObject.get("faces")
+
+    # 
+    if(cfrrescode == 200):
+        for list in jsonArray:
+            print (list.get("emotion").get("value"))
+            print(csrresponse.text)
+
+            data = list.get("emotion").get("value")
+
+        #@app.route('/')
+        #def index():
+        #    return emotion
+    else:
+        print("Error : " + cfrresponse.text)
+
+    return render_template("testapi.html", data = data)
---
+++ server/db/mysql.py
@@ -0,0 +1,16 @@
+import pymysql
+
+class Testdb:
+    def __init__(self):
+        self.db = pymysql.connect(host='localhost',
+                                port=3306, user='*', passwd='*',
+                                db='*', charset='utf8')
+
+    #passwd는 일단 비공개를 위해 가렸습니다.
+
+    def select_all(self):
+        cursor = self.db.cursor()
+        sql = 'select * from TEST.test_table'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result

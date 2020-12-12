23390b48f149ee86544932f5ee9252b1ef305cbf
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-12 18:26:06 +0900

    feat: webcam, audio axios API 정의

--- client/src/common/api.js
+++ client/src/common/api.js
@@ -9,4 +9,18 @@ const getTextMusicData = async (text) => {
     return response.data;
 }
 
-export { getTextMusicData };
+const getVideoMusicData = async (image) => {
+    let form = new FormData();
+    form.append('file', image, 'image.png');
+    const response = await axios.post('/testApi3', form);
+    return response.data;
+}
+
+const getAudioMusicData = async (audio) => {
+    let form = new FormData();
+    form.append('file', audio, 'audio.mp3');
+    const response = await axios.post('/testApi1', form);
+    return response.data;
+}
+
+export { getTextMusicData, getVideoMusicData, getAudioMusicData };

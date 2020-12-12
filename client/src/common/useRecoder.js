b6d8ba0994c247b293afb211e0f14f005c7ffd57
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-12 18:26:30 +0900

    feat: useRecoder 정의
    - audio를 구현하기 위한 useRecoder 구현

---
+++ client/src/common/useRecoder.js
@@ -0,0 +1,52 @@
+/* https://codesandbox.io/s/81zkxw8qnl?file=/src/useRecorder.js:0-1238 */
+
+import { useEffect, useState } from "react";
+
+const useRecorder = () => {
+  const [audioURL, setAudioURL] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [recorder, setRecorder] = useState(null);
+  const [blobObject, setBlobObject] = useState(null);
+
+  useEffect(() => {
+    // Lazily obtain recorder first time we're recording.
+    if (recorder === null) {
+      if (isRecording) {
+        requestRecorder().then(setRecorder, console.error);
+      }
+      return;
+    }
+
+    // Manage recorder state.
+    if (isRecording) {
+      recorder.start();
+    } else {
+      recorder.stop();
+    }
+
+    // Obtain the audio when ready.
+    const handleData = e => {
+      setBlobObject(e.data);
+      setAudioURL(URL.createObjectURL(e.data));
+    };
+
+    recorder.addEventListener("dataavailable", handleData);
+    return () => recorder.removeEventListener("dataavailable", handleData);
+  }, [recorder, isRecording]);
+
+  const startRecording = () => {
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+  };
+
+  return [audioURL, isRecording, startRecording, stopRecording, blobObject];
+};
+
+async function requestRecorder() {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  return new MediaRecorder(stream);
+}
+export default useRecorder;

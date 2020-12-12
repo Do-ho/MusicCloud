fa94c8676e5bb357a37e3e11275cecc951f33b16
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-12 18:47:54 +0900

    feat: api 명세 변경에 따른 api 변경

--- client/src/common/api.js
+++ client/src/common/api.js
@@ -5,21 +5,21 @@ axios.defaults.baseURL = "http://127.0.0.1:5000";
 const getTextMusicData = async (text) => {
     let form = new FormData();
     form.append('text', text);
-    const response = await axios.post('/testApi2', form);
+    const response = await axios.post('/recomend/transfer/text-to-music', form);
     return response.data;
 }
 
 const getVideoMusicData = async (image) => {
     let form = new FormData();
     form.append('file', image, 'image.png');
-    const response = await axios.post('/testApi3', form);
+    const response = await axios.post('/recomend/transfer/face-to-music', form);
     return response.data;
 }
 
 const getAudioMusicData = async (audio) => {
     let form = new FormData();
     form.append('file', audio, 'audio.mp3');
-    const response = await axios.post('/testApi1', form);
+    const response = await axios.post('/recomend/transfer/voice-to-music', form);
     return response.data;
 }
 
